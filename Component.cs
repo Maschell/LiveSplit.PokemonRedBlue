96f394b55a7a97ba2067c49ccd72da71f02c9ade
Maschell <Maschell@gmx.de>
2014-08-28 19:59:09 +0200

    First

---
+++ Component.cs
@@ -0,0 +1,57 @@
+﻿#define GAME_TIME
+
+//using LiveSplit.ASL;
+using LiveSplit.Model;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+
+namespace LiveSplit.UI.Components
+{
+    class Component : LogicComponent
+    {
+        //public ComponentSettings Settings { get; set; }
+
+        public override string ComponentName
+        {
+            get { return "Pokemon Game Time"; }
+        }
+
+
+        public Component()
+        {
+        }
+
+        public override void Update(IInvalidator invalidator, LiveSplitState state, float width, float height, LayoutMode mode)
+        {
+        }
+
+        public override System.Xml.XmlNode GetSettings(System.Xml.XmlDocument document)
+        {
+            return document.CreateElement("x");
+        }
+
+        public override System.Windows.Forms.Control GetSettingsControl(UI.LayoutMode mode)
+        {
+            return null;
+        }
+      
+        
+public override void Dispose()
+{
+ 	throw new NotImplementedException();
+}
+
+public override void RenameComparison(string oldName, string newName)
+{
+ 	throw new NotImplementedException();
+}
+
+public override void SetSettings(System.Xml.XmlNode settings)
+{
+ 	throw new NotImplementedException();
+}
+}
+
+    }
+}
---
+++ Factory.cs
@@ -0,0 +1,57 @@
+﻿using LiveSplit.Model;
+using LiveSplit.UI.Components;
+using System;
+
+[assembly: ComponentFactory(typeof(Factory))]
+
+namespace LiveSplit.UI.Components
+{
+    public class Factory : IComponentFactory
+    {
+        public string ComponentName
+        {
+            get { return "Pokemon Game Time"; }
+        }
+
+        public ComponentCategory Category
+        {
+            get { return ComponentCategory.Control; }
+        }
+
+        public string Description
+        {
+            get { return "Provides Game Time for Pokemon Red on BizHawk."; }
+        }
+
+        public IComponent Create(LiveSplitState state)
+        {
+            return new Component();
+        }
+
+        public string UpdateName
+        {
+            get { return ComponentName; }
+        }
+
+        public string XMLURL
+        {
+#if RELEASE_CANDIDATE
+#else
+            get { return "http://livesplit.org/update/Components/update.LiveSplit.Pokemon.xml"; }
+#endif
+        }
+
+        public string UpdateURL
+        {
+#if RELEASE_CANDIDATE
+#else
+            get { return "http://livesplit.org/update/"; }
+#endif
+        }
+
+        public Version Version
+        {
+            get { return Version.Parse("1.0.0"); }
+        }
+    }
+}
